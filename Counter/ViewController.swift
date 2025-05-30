26f03c7511e28d0c017d89fdcfae42e3c4afc1bd
BeerHobbit <kovalenko537@gmail.com>
2025-05-30 21:42:20 +0300

    Внесены исправления в ViewController в соответствие с ревью

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -1,33 +1,44 @@
-//
-//  ViewController.swift
-//  Counter
-//
-//  Created by Артём Коваленко on 30.05.2025.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
-
-    @IBOutlet weak var countValueLabel: UILabel!
-    @IBOutlet weak var countHistoryTextView: UITextView!
+final class ViewController: UIViewController {
+    
+    @IBOutlet private weak var countValueLabel: UILabel!
+    @IBOutlet private weak var countHistoryTextView: UITextView!
     
     private var count: Int = 0 {
-        // Обновление счетчика при изменении переменной
         didSet {
             countValueLabel.text = "Значение счетчика:\n\(count)"
         }
     }
     
-    // Действие на кнопку +
-    @IBAction func addOnTap(_ sender: Any) {
+    private lazy var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd.MM.yy HH:mm:ss"
+        return formatter
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        countValueLabel.text = "Значение счетчика:\n\(count)"
+    }
+    
+    private func makeCurrentDateString() -> String {
+        let currentDate = Date()
+        return dateFormatter.string(from: currentDate)
+    }
+    
+    private func scrollToLastString() {
+        let range = NSMakeRange(countHistoryTextView.text.count, 0)
+        countHistoryTextView.scrollRangeToVisible(range)
+    }
+    
+    @IBAction private func addOnTap(_ sender: Any) {
         count += 1
         countHistoryTextView.text += "\n[\(makeCurrentDateString())]: Значение изменено на +1"
         scrollToLastString()
     }
     
-    // Действие на кнопку -
-    @IBAction func substractOnTap(_ sender: Any) {
+    @IBAction private func substractOnTap(_ sender: Any) {
         if count > 0 {
             count -= 1
             countHistoryTextView.text += "\n[\(makeCurrentDateString())]: Значение изменено на -1"
@@ -38,32 +49,10 @@ class ViewController: UIViewController {
         }
     }
     
-    // Действие на кнопку сброса
-    @IBAction func resetOnTap(_ sender: Any) {
+    @IBAction private func resetOnTap(_ sender: Any) {
         count = 0
         countHistoryTextView.text += "\n[\(makeCurrentDateString())]: Значение сброшено"
         scrollToLastString()
     }
-    
-    // Функция, возвращающая строку форматированной даты
-    private func makeCurrentDateString() -> String {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "dd.MM.yy HH:mm:ss"
-        let currentDate = Date()
-        return formatter.string(from: currentDate)
-    }
-    
-    //Функция, скроллящая до последнего символа строки
-    private func scrollToLastString() {
-        let range = NSMakeRange(countHistoryTextView.text.count, 0)
-        countHistoryTextView.scrollRangeToVisible(range)
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        countValueLabel.text = "Значение счетчика:\n\(count)"
-    }
-
 }
 
